f2eb913671ea84a88bac815449007f76c2c6fcb7
League Student <league@iMac-5.attlocal.net>
2020-02-02 12:29:46 -0800

    ____

--- src/_06_for_loop_gauntlet/ForLoopGauntlet.java
+++ src/_06_for_loop_gauntlet/ForLoopGauntlet.java
@@ -25,7 +25,7 @@ for(int b = 0; b < 778; b++) {
 	}
 }
 int age = 0;
-for(int t = 2006; t < 2020; t++, age++) {
+for(int t = 2006; t < 2021; t++, age++) {
 	System.out.println(t + "   I am "+age+" years old.");
 }
 int n = 0;
@@ -41,6 +41,45 @@ for(int c = 0; c < 9; c++) {
 	}
 	
 }
+}
+//1
+for(int b = 0; b < 10; b++, b++, b++) {
+	if (b >= 0 & b < 4) {
+	System.out.println(0);
+	}
+	else if (b > 3 & b < 7) {
+	System.out.print(0);
+	System.out.println(1);
+	System.out.print(1);
+	System.out.println(1);
+	System.out.print(2);
+	System.out.println(1);
+	
+	}
+	else {
+		System.out.println("2");
+		System.out.println("2");
+		System.out.println("2");
+	}
+}
+//2
+for(int r = 1; r < 10; r++) {
+System.out.print(r+" ");
+if(r%3==0) {
+	System.out.println();
+}
+}
+//3
+for(int u = 0; u < 101; u++) {
+	if(u < 10) {
+		System.out.print(u + "  ");
+	}
+	else {
+	System.out.print(u + " ");
+	}
+	if(u%10==0) {
+		System.out.println();
+	}
 }
 }
 }
